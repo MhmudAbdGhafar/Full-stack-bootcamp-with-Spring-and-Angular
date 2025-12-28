91e421a2fbc5d571c7b7d7d09709989cc620b56f
Mahmoud Abdul Ghaffar <mghaffar@tmcmf.com>
2025-12-28 19:42:14 +0200

    [fix] remove all unneeded files in the repo

--- Session 3 - Task/src/com/ioc/example/task1/Draw3dShape.java
+++
@@ -1,8 +0,0 @@
-package com.ioc.example.task1;
-
-public class Draw3dShape {
-
-    void draw(String shapeName){
-        System.out.printf("Drawing %s as 3D Shape\n", shapeName);
-    }
-}
--- Session 4 - Task/src/com/bean/scopes_lifecycle/Draw2DShape.java
+++
@@ -1,9 +0,0 @@
-package com.bean.scopes_lifecycle;
-
-public class Draw2DShape {
-
-    public void draw(String shapeName){
-        System.out.printf("Drawing %s as 2D Shape\n", shapeName);
-    }
-
-}
